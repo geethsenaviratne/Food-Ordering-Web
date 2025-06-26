57a0e194d9e2af7a6de8ba50b2e6d62d1f2c383e
Geeth Seneviratne <it23226128@my.sliit.lk>
2025-06-26 12:34:00 +0530

    Add RestaurantService. java and CreateRestaurantRequest.java files

---
+++ src/main/java/com/my/food_ordering/request/CreateRestaurantRequest.java
@@ -0,0 +1,20 @@
+package com.my.food_ordering.request;
+
+import com.my.food_ordering.model.Address;
+import com.my.food_ordering.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateRestaurantRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+
+}
---
+++ src/main/java/com/my/food_ordering/service/RestaurantService.java
@@ -0,0 +1,27 @@
+package com.my.food_ordering.service;
+
+
+import com.my.food_ordering.dto.RestaurantDto;
+import com.my.food_ordering.model.Restaurant;
+import com.my.food_ordering.model.User;
+import com.my.food_ordering.request.CreateRestaurantRequest;
+
+import java.util.List;
+
+public interface RestaurantService {
+    public Restaurant createRestaurant(CreateRestaurantRequest req, User user);
+
+    public Restaurant updateRestaurant(Long restaurantId, CreateRestaurantRequest updateRestaurantRequest)throws Exception;
+
+    public void deleteRestaurant(Long restaurantId)throws Exception;
+
+    public List<Restaurant> getAllRestaurant();
+
+    public List<Restaurant> searchRestaurant();
+
+    public Restaurant findRestaurantById(Long id)throws Exception;
+
+    public Restaurant getRestaurantByUserId(long userId)throws Exception;
+
+    public RestaurantDto addToFavorites(Long restaurantId, User user)throws Exception;
+}
